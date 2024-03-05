7bfe3b57cc7b90b9bc7858c7c321128d62cbe1e2
Maikon <michaelmqvu@gmail.com>
2024-03-04 20:31:10 -0800

    fix?

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.CoAAtMGO.js","_app/immutable/chunks/disclose-version.h_rUUzP8.js","_app/immutable/chunks/runtime.DPct-Ydm.js"];
-export const stylesheets = ["_app/immutable/assets/0.CLAz9Ws2.css"];
-export const fonts = [];
